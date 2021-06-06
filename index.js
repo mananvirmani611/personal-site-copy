fbe2d1aaa2ded7c962541547b2eee0fb4bb2023b
Manan <mananvirmani611@gmail.com>
2021-06-06 20:41:34 +0530

    responsiveness to be added

---
+++ index.js
@@ -0,0 +1,7 @@
+if(screen.width<750){
+  document.getElementById("left-div").style.fontSize = "32px";
+  document.getElementById("right-div").style.fontSize = "32px";
+  document.querySelector(".welcome-text-paragraph").style.fontSize = "50px";
+  document.querySelector(".welcome-text-paragraph-2").style.fontSize = "32px";
+
+}
