f49a025b6d2bee483bab2602560a315f1a0ef7c7
Manan <mananvirmani611@gmail.com>
2021-06-06 21:40:57 +0530

    icons and button size enlarged

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 if(screen.width<750){
-  document.getElementById("left-div").style.fontSize = "32px";
-  document.getElementById("right-div").style.fontSize = "32px";
+  document.getElementById("left-div").style.fontSize = "36px";
+  document.getElementById("right-div").style.fontSize = "36px";
+  document.getElementById("green-button-1").style.fontSize = "36px";
+  document.getElementById("green-button-2").style.fontSize = "36px";
+
   document.querySelector(".welcome-text-paragraph").style.fontSize = "50px";
   document.querySelector(".welcome-text-paragraph-2").style.fontSize = "32px";
 
